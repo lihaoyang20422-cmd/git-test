28099dabb77f0acc54c24d6ad319f3f429dd08a7
LHY <yang@163.com>
2026-08-18 14:52:53 +0800

    将主功能封装为fun3

--- project/mian.c
+++ project/mian.c
@@ -10,19 +10,21 @@ void fun2(void)
     printf("si mi ma sai! \n");
 }
 
+void fun3(void)
+{
+    int a = 10;
+    int b = 0;
+    printf("a + b = %d", a + b);
+}
 
 int main(void)
 {
     fun1();
     fun2();
+    fun3();
 
-    int a = 10;
-    int b = 0;
-    printf("a + b = %d", a + b);
-    while(1)
+    while (1)
     {
         printf("hello world ! \n");
     }
-
 }
-
