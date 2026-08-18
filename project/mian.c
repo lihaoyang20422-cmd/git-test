1560875b25fe34ce9f161a1f98d931d9218d6ffe
LHY <yang@163.com>
2026-08-18 14:46:18 +0800

    添加fun2

--- project/mian.c
+++ project/mian.c
@@ -5,10 +5,16 @@ void fun1(void)
     printf("ni hao! \n");
 }
 
+void fun2(void)
+{
+    printf("si mi ma sai! \n");
+}
+
 
 int main(void)
 {
     fun1();
+    fun2();
     while(1)
     {
         printf("hello world ! \n");
